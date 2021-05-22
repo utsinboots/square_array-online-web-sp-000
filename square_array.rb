1fbc65207556153d8ff66cdc7b438b46392697e7
Utshant Gurung <utshantgurung@gmail.com>
2021-05-22 15:46:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ require 'pry'
 def square_array(array)
   new_array = []
   array.each do |array_element|
-  new_array << [array_element**array_element]
+  new_array << [array_element**2]
   binding.pry
   end
   new_array 
